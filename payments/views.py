616c295fc9ab87f2ba702ec89188fc0c4e7d1dad
sergei <you@example.com>
2024-02-10 21:14:14 +0800

    homework 5

--- payments/views.py
+++ payments/views.py
@@ -5,7 +5,7 @@ from rest_framework.filters import OrderingFilter
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 
-from application.services import get_session, retrieve_session
+from payments.services import get_session, retrieve_session
 from .models import Payment
 from .serializers import PaymentSerializer, PaymentCreateSerializer
 
